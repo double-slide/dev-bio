e3c1d4afbb4fb7862e68dfeb2273114be10906d3
Iaan Johnston <iaan.e.jo@gmail.com>
2022-06-06 11:09:24 -0700

    Initial setup & image upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src='https://media-exp1.licdn.com/dms/image/C5603AQHwuNX81FzwEQ/profile-displayphoto-shrink_800_800/0/1646859896622?e=1660176000&v=beta&t=R7Qb7oLtUb6GhcM8gpE9tiQ2jbMiB_7R5f9G-eHfXos' alt='headshot'/>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
