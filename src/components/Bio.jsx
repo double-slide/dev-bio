7d21f4105457335b9ec242bf80a79bb18cbaf294
Iaan Johnston <iaan.e.jo@gmail.com>
2022-06-07 13:40:08 -0700

    Bio card updates

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,26 +1,38 @@
-import { Card, Grid } from '@mantine/core';
+import { Card, Center, Grid } from '@mantine/core';
 
 function Bio() {
   return (
     <>
-    <Grid
-    // gutter='xl'
-    >
-      <Card
-      shadow='lg'
+        <Card
+          shadow='xl'
+        >
+      <Grid
+        gutter='xl'
       >
-        
-        <img 
-        // caption='Iaan'
-        // radius='lg'
-        height={100}
-        src='https://media-exp1.licdn.com/dms/image/C5603AQHwuNX81FzwEQ/profile-displayphoto-shrink_800_800/0/1646859896622?e=1660176000&v=beta&t=R7Qb7oLtUb6GhcM8gpE9tiQ2jbMiB_7R5f9G-eHfXos' 
-        alt='headshot'
-        />
 
-      </Card>
+          <Grid.Col span={4}>
 
-    </Grid>
+          </Grid.Col>
+          <Grid.Col span={4}>
+
+          </Grid.Col>
+          <Grid.Col span={4}>
+
+<Center>
+            <img
+              // caption='Iaan'
+              // radius='lg'
+              height={100}
+              src='https://media-exp1.licdn.com/dms/image/C5603AQHwuNX81FzwEQ/profile-displayphoto-shrink_800_800/0/1646859896622?e=1660176000&v=beta&t=R7Qb7oLtUb6GhcM8gpE9tiQ2jbMiB_7R5f9G-eHfXos'
+              alt='headshot'
+            />
+</Center>
+
+
+          </Grid.Col>
+      </Grid>
+        </Card>
+
     </>
   );
 }
